0276569ab310559c1a690b0bea5a7d5284e856c1
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-15 19:50:12 -0400

    prompted user for name
    and alerted

--- GoToTrainingWeek2/js/week2.js
+++ GoToTrainingWeek2/js/week2.js
@@ -6,3 +6,9 @@ GoTo Training Week #2
  */
 
 //alert("Test");
+
+//Create an age calculator
+var name = prompt("Let's figure out how old you are!\nPlease type in your name");
+//Use the variable and say hello
+alert("Welcome "+name+"!");
+
