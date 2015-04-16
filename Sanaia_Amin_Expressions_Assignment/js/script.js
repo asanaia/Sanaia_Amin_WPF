8e17ff1d52e329ee7d226e4fafc2ef0e0d45cc37
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-15 23:23:04 -0400

    Prompt User for Name, Tolls
    prompt for name and number of tolls and checked it in console log

--- Sanaia_Amin_Expressions_Assignment/js/script.js
+++ Sanaia_Amin_Expressions_Assignment/js/script.js
@@ -1,9 +1,18 @@
 
  /*
 Amin Sanaia
-SDI Section #
+SDI Section #1
 Expressions Assignment
 4-15-2015
  */
 
- alert("Test");
+//alert("Test");
+
+//Create Calculator for Monthly Tolls Expenses
+ var intro = prompt("We can help you calculate your tolls expenses\nLet's get started!\nWhat is your name?");
+
+ alert("Hello "+intro);
+
+ //Ask the user how many tolls do you pass by daily
+ var numberOfTolls = prompt("How many tolls do you pass daily?");
+ console.log(numberOfTolls);
