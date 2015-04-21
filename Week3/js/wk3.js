72e372dfe2d3918a84ca0d8e6c92bc41ef21e6d0
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-21 13:51:32 -0400

    week3 relational expression
    week3 relational expression

--- Week3/js/wk3.js
+++ Week3/js/wk3.js
@@ -8,10 +8,11 @@ Week3
 
 // alert("test")
 
- var oldEnough = false;
+ var kidHeight = 40;
+ var minHeight = 48
  //if the child is old enough, print to the console "you can ride!"
- if(oldEnough){
+ //if the kid is over 48 inches in height
+ if(kidHeight > minHeight){
   //code performed if condition is true
   console.log("You can ride the coaster!");
  }
- console.log("what comes after");
