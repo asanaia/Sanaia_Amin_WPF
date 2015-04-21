4a070bfca9290506178ff2ac97f7e42b876fb622
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-21 13:45:11 -0400

    Created folder for week 3 practice
    Created folder for week 3 practice

---
+++ Week3/js/wk3.js
@@ -0,0 +1,16 @@
+
+ /*
+Amin Sanaia
+SDI Section #1
+Week3
+4-21-2015
+ */
+
+// alert("test")
+
+ var oldEnough = true;
+ //if the child is old enough, print to the console "you can ride!"
+ if(oldEnough){
+  //code performed if condition is true
+  console.log("You can ride the coaster!");
+ }
