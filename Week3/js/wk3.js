a09ca8a8a309d84a3b52b88d272bbe2c75ca74c4
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-21 13:47:25 -0400

    Week3 Conditionals example
    Week3 Conditionals example

--- Week3/js/wk3.js
+++ Week3/js/wk3.js
@@ -8,9 +8,10 @@ Week3
 
 // alert("test")
 
- var oldEnough = true;
+ var oldEnough = false;
  //if the child is old enough, print to the console "you can ride!"
  if(oldEnough){
   //code performed if condition is true
   console.log("You can ride the coaster!");
  }
+ console.log("what comes after");
