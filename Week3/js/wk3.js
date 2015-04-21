dfc4a80adf62864b508fce5590e3b6d50accf1a2
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-21 14:34:03 -0400

    week3 else if practice
    week3 else if practice

--- Week3/js/wk3.js
+++ Week3/js/wk3.js
@@ -8,15 +8,19 @@ Week3
 
 // alert("test")
 
- var kidHeight = 48;
+ var kidHeight = 46;
  var minHeight = 48;
+ var wParentHeight = 45; //the height of the kid with the parent
 
  //if the child is old enough, print to the console "you can ride!"
  //if the kid is over 48 inches in height
- if(kidHeight > minHeight){
-  //code performed if condition is true
+ if(kidHeight > minHeight) {
+  //you can ride!
   console.log("You can ride the coaster!");
+ }else if(kidHeight > wParentHeight){
+  //you can ride with a parent present
+  console.log("You can ride, but only with a parent present.");
  }else{(kidHeight <= minHeight)
-  //code performed if condition is false
+  //sorry you have growing to do
   console.log("Sorry kid, you've got some growing to do first!");
 }
