af5ce2e692825a7dfd56adbd0bccb1ce281786df
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-22 19:44:10 -0400

    found pear number old school way
    found pear number old school way

--- GoTo TrainingWeek3/js/gotoWeek3.js
+++ GoTo TrainingWeek3/js/gotoWeek3.js
@@ -22,3 +22,49 @@ GoTo Training Week #3
  //Count the number of pears in our fruit bowl
  //Create a variable that will keep track of how many pears there are
  var totalNumPears = 0;
+ //test each item in the array and check if it is a pear
+ //basic structure of a conditional
+ //if (condition to test) {code that will run if true}
+
+ //"7" == 7 - true
+ //"7" === 7 - false
+ //prompts always return strings!!
+ if(bowlOfFruit[0] === "pear"){
+//This code will run if true
+  //Add 1 to the total numbers of pears
+  console.log("This item is a pear!");
+  totalNumPears++;  //totalNumPears = totalNumPears + 1;
+ }else{
+  console.log("This fruit is not a pear!");
+ }
+ if(bowlOfFruit[1] === "pear"){
+//This code will run if true
+  //Add 1 to the total numbers of pears
+  console.log("This item is a pear!");
+  totalNumPears++;  //totalNumPears = totalNumPears + 1;
+ }else{
+  console.log("This fruit is not a pear!");
+ }
+ if(bowlOfFruit[2] === "pear"){
+//This code will run if true
+  //Add 1 to the total numbers of pears
+  console.log("This item is a pear!");
+  totalNumPears++;  //totalNumPears = totalNumPears + 1;
+ }else{
+  console.log("This fruit is not a pear!");
+ }
+ if(bowlOfFruit[3] === "pear"){
+//This code will run if true
+  //Add 1 to the total numbers of pears
+  console.log("This item is a pear!");
+  totalNumPears++;  //totalNumPears = totalNumPears + 1;
+ }else{
+  console.log("This fruit is not a pear!");
+ }
+
+ console.log("There are "+totalNumPears+" pear(s) in the fruit bowl.")
+
+ //Create another variable to track pears
+ var pearNumber = 0;
+ //Creat a loop for repetitive code
+ // For Loop Structure
