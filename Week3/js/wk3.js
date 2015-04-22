bdcb0b739a4eb5c94c3cf9ae8a8ac13949ac7877
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-22 17:09:33 -0400

    week3 Logical Operators
    week3 Logical Operators

--- Week3/js/wk3.js
+++ Week3/js/wk3.js
@@ -8,19 +8,15 @@ Week3
 
 // alert("test")
 
- var kidHeight = 46;
- var minHeight = 48;
- var wParentHeight = 45; //the height of the kid with the parent
+ //Conditional Logic - Logic Operators
 
- //if the child is old enough, print to the console "you can ride!"
- //if the kid is over 48 inches in height
- if(kidHeight > minHeight) {
-  //you can ride!
-  console.log("You can ride the coaster!");
- }else if(kidHeight > wParentHeight){
-  //you can ride with a parent present
-  console.log("You can ride, but only with a parent present.");
- }else{(kidHeight <= minHeight)
-  //sorry you have growing to do
-  console.log("Sorry kid, you've got some growing to do first!");
-}
+ var budget = 300;
+ var iPhonePrice = 199.99;
+ var wonLottery = true;
+
+ //if the price of the phone is less of our budget
+ if(iPhonePrice < budget ||  wonLottery ){
+  console.log("We can by the phone!");
+ }else{
+  console.log("No phone for you!!");
+ }
