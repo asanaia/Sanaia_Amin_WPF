3e512bb3124bcb68a0a6fffa735ed70dd9f286ad
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-22 21:51:44 -0400

    Ask For Name
    Ask for name and validate entry not left blank

--- Sanaia_Amin_Conditionals_Assignment/js/js.js
+++ Sanaia_Amin_Conditionals_Assignment/js/js.js
@@ -7,4 +7,15 @@ Conditionals Assignment
  */
 
 // alert("Test");
+//Create Calculator to figure out how many movies in collection start with letter M
+ var intro = prompt("Lets Figure out how many movies in your collection begin with letter M \nWhat is your name?");
+ if(name === ""){
+  //If the user leaves it blank it will prompt to enter a value
+  name = prompt("Please do not leave blank.\n What is your name?");
+ }
+
+ //Print to console
+ console.log(name)
+
  
+
