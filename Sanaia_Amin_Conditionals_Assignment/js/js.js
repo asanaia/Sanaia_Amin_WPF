23724dc5da2159a44f9516f924daf80dc9b69f27
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-22 23:41:55 -0400

    Changed Calculator
    Changed calculator to figure out if you have enough money for vacation
    after expenses

--- Sanaia_Amin_Conditionals_Assignment/js/js.js
+++ Sanaia_Amin_Conditionals_Assignment/js/js.js
@@ -7,15 +7,17 @@ Conditionals Assignment
  */
 
 // alert("Test");
-//Create Calculator to figure out how many movies in collection start with letter M
- var intro = prompt("Lets Figure out how many movies in your collection begin with letter M \nWhat is your name?");
+//Create Calculator to figure out how if you have enough money for vacation after expenses paid
+ var intro = prompt("Lets Figure out how if you have enough money for vacation after monthly expenses \nWhat is your name?");
  if(name === ""){
   //If the user leaves it blank it will prompt to enter a value
   name = prompt("Please do not leave blank.\n What is your name?");
  }
 
  //Print to console
- console.log(name)
-
- 
+ console.log(name);
+//Array of Monthly Expenses
+ var monthlyExpenses = ["Rent", "Utilities", "Car", "Loan Payments"];
+ //Cosole log movieTitles
+ console.log(monthlyExpenses);
 
