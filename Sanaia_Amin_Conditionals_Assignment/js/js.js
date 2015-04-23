2701a3a2062056a38a2078c3a95cee0ae1bbcc59
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-23 00:29:23 -0400

    Calculated monthly expenses
    prompted user for expenses and calculated monthly expenses

--- Sanaia_Amin_Conditionals_Assignment/js/js.js
+++ Sanaia_Amin_Conditionals_Assignment/js/js.js
@@ -18,6 +18,31 @@ Conditionals Assignment
  console.log(name);
 //Array of Monthly Expenses
  var monthlyExpenses = ["Rent", "Utilities", "Car", "Loan Payments"];
- //Cosole log movieTitles
+ //Cosole log monthlyExpenses
  console.log(monthlyExpenses);
+
+ //Ask Expenses amounts (I verified that values were showing on the console)
+ var rent = prompt("What is the cost of your rent monthly?");
+ console.log(rent+ " dollars");
+ var utilities = prompt("What is the cost of your utilities monthly?");
+ console.log(utilities+ " dollars");
+ var car = prompt("What is the cost of your car monthly?");
+ console.log(car+ " dollars");
+ var loanPayments = prompt("What is the cost of your loan payments monthly?");
+ console.log(loanPayments+ " dollars");
+
+//I entered for rent-1000 / utilities-200 / car-500 / loan-300 and assigned them below:
+ var rent = 1000;
+ console.log(rent+ " dollars");
+ var utilities = 200;
+ console.log(utilities+ " dollars");
+ var car = 500;
+ console.log(car+ " dollars");
+ var loanPayments = 300;
+ console.log(loanPayments+ " dollars");
+
+ //Calculate Total Monthly Expenses
+ var totalMonthlyExpenses = rent + utilities + car + loanPayments;
+ console.log(totalMonthlyExpenses+ " dollars");
+ //Monthly expenses total to 2000
 
