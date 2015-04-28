43981fb0bbf8dc5d8127658813fc2360b4271d76
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-28 15:42:57 -0400

    created functions worksheet folder
    created folder and completed circumference example

---
+++ Functions_worksheet/js/js.js
@@ -0,0 +1,17 @@
+
+ /*
+Amin Sanaia
+SDI Section #1
+Functions Worksheet
+4-28-2015
+ */
+
+// alert("Test");
+
+ //Calculate the circumference of a circle
+ var radius = 6
+ var circumference = calculateCircumference(radius);
+ function calculateCircumference(radius){
+  return (radius * 3.14 * 2);
+ }
+ console.log("The Circumference of the circle is " + circumference);
