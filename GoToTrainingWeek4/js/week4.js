c77d0d049c140f87ab4f80866c1a2c1d44753c96
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-29 19:31:10 -0400

    prompted for min number and validate using loop
    prompted for min number and validate using loop

--- GoToTrainingWeek4/js/week4.js
+++ GoToTrainingWeek4/js/week4.js
@@ -6,4 +6,22 @@ GoTo Training Week #4
 4-29-2015
  */
 
- alert("test");
+ //alert("test");
+ //Create a file that will give us a random number between two numbers
+ //Ask the user for the min number
+ var minNum = prompt("Let's find a number between two numbers!\n What is the min number?");
+ //validate the prompt is not blank and it is a number
+ while(minNum===""|| isNaN(minNum)){
+  //re prompt the user
+  minNum = prompt("Please do not leave blank and only use numbers\n What is the min number?");
+ }
+/*
+Basic structure of a while loop
+//Declare a variable
+var num = prompt("type in a number");
+
+while( x > 6 ) {  //condition to test
+//some sort of change to the variable in our condition to test.
+x++;
+}
+ */
