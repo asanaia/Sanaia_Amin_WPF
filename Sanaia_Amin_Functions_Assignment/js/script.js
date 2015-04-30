3f9c28742491a0cbc76a0cd91794e3d3ffdd8957
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-29 22:55:33 -0400

    Functions Assignment prompted for destination distance
    Functions Assignment prompted for destination distance, used while loop
    to make sure it was not left blank and it is a number entered

--- Sanaia_Amin_Functions_Assignment/js/script.js
+++ Sanaia_Amin_Functions_Assignment/js/script.js
@@ -9,14 +9,23 @@ Functions Assignment
  //alert("test");  Link Tested
 
  //Let's Make a calculator to see how long it will take to get to another city on a trip
-
  var name = prompt("Lets Figure out how long it will take to get to another city on a trip \nWhat is your name?");
  //validate the prompt is not blank
  while(name===""){
   //re prompt the user
   name = prompt("Please do not leave blank \n What is your name?");
  }
+ console.log(name);
+ //validated with loop
 
- var distance = prompt("");
+ //Prompt for distance to another city and validate with loop
+ var distance = prompt("What is the distance to the destination city?");
+ while(distance===""  ||  isNaN(distance)){
+  //re prompt the user validate the prompt is not bland and is a number
+  distance = prompt("Please do not leave blank and make sure it is a number\n What is the distance to the destination city?");
+ }
+ console.log("The total distance to your destination is "+distance+" miles");
+
+ 
 
 
