ee295333774e3092ddf1bc93c5d515bb76105667
asanaia <Amin@Amins-MBP.attlocal.net>
2015-04-29 22:37:48 -0400

    Functions Assignment added prompt for name
    Functions Assignment added prompt for name and validate with loop

--- GoToTrainingWeek4/js/week4.js
+++ GoToTrainingWeek4/js/week4.js
@@ -67,4 +67,3 @@ x++;
   return randomNumber;
  }
 
- 
--- Sanaia_Amin_Functions_Assignment/js/script.js
+++ Sanaia_Amin_Functions_Assignment/js/script.js
@@ -6,5 +6,17 @@ Functions Assignment
 4-29-2015
  */
 
- //alert("test");
+ //alert("test");  Link Tested
+
+ //Let's Make a calculator to see how long it will take to get to another city on a trip
+
+ var name = prompt("Lets Figure out how long it will take to get to another city on a trip \nWhat is your name?");
+ //validate the prompt is not blank
+ while(name===""){
+  //re prompt the user
+  name = prompt("Please do not leave blank \n What is your name?");
+ }
+
+ var distance = prompt("");
+
 
